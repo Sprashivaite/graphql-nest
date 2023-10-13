481e0cbcf6f0d72c31f27249db89209119d60b54
Arkadiy Telitsyn <sanitar.kem@mail.ru>
2023-10-13 20:15:56 +0500

    add docker and ci pipeline

---
+++ src/auth/auth.controller.spec.ts
@@ -0,0 +1,29 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { UserRepository } from './user.repository';
+
+describe('AuthService', () => {
+  let authService: AuthService;
+  let jwtService: JwtService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        JwtService,
+        {
+          provide: UserRepository,
+          useValue: {},
+        },
+      ],
+    }).compile();
+
+    authService = module.get<AuthService>(AuthService);
+    jwtService = module.get<JwtService>(JwtService);
+  });
+
+  it('should be defined', () => {
+    expect(authService).toBeDefined();
+  });
+});
